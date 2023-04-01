b04d2bbc5d37ea5904da79c5839b5ad4fdda1847
Jesus Coloma <colomacardoza16@gmail.com>
2023-03-31 20:20:59 -0500

    Button start already work, I added the level indicator also the logical of game already work

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,7 +1,7 @@
 const contanier=document.getElementById('container');
-const colors=document.querySelectorAll('.color');
+const colors=document.querySelectorAll('.button--color');
+const level=document.getElementById('level');
 
-const arrayColors=Array.from(colors);
 
 //sounds
 let click=new Audio('./sounds/click.wav');
@@ -13,10 +13,14 @@ let randomColors=[];
 let randomNumbers=[];
 let round;
 let touch;
+let arrayColors=[];
 
+//initiation of variables
 round=1;
 touch=0;
-const mixtColors=()=>{
+arrayColors=Array.from(colors);
+
+const mixColors=()=>{
     for (let i = 0; i <round ; i++) {
         const randomNum=parseInt(Math.random()*4); //to 0 to 3
         randomNumbers.push(randomNum);
@@ -25,53 +29,58 @@ const mixtColors=()=>{
 
 
 const showColors=()=>{
+    //First We disable the buttons to prevent click events 
+    arrayColors.map(color=>{color.disabled=true});
+    //We show the color serie
     i=-1
     let initShowColors= setInterval(()=>{
         setTimeout(()=>{            
-            arrayColors[randomNumbers[i]].classList.add('color--select');
+            arrayColors[randomNumbers[i]].classList.add('button--select');
             // console.log(arrayColors[randomNumbers[i]],i);
         },500);
         setTimeout(()=>{
-            arrayColors[randomNumbers[i]].classList.remove('color--select');
+            arrayColors[randomNumbers[i]].classList.remove('button--select');
             // console.log(arrayColors[randomNumbers[i]],i);
         },1000);
         i++;
         if (i==round-1) {
             clearInterval(initShowColors)
+            //we active the buttons to can play
+            arrayColors.map(color=>{color.disabled=false});
         }
         click.play()
     },1500)
 }
 
 contanier.addEventListener('click',(e)=>{
-    if (e.target.classList.contains('color')) {
+    if (e.target.classList.contains('button--color')) {
         if (e.target.id==arrayColors[randomNumbers[touch]].id) {
             console.log(e.target.id);
             right.play();
+            touch++ 
+            if (touch==round) {
+                //We win a round
+                reset(round+1);//we reset but increase the level
+            }
         }else{
+            //The game Finish
             wrong.play();
-            //The play Finish, we reset all 
-            touch=0; //we reset the touch to the new round
-            round=1;//It increase the level
-            randomNumbers=[];//reset 
-            // mixtColors()
-            // showColors()
+            reset(1);//we reset all included the level 
             console.log('You lose, Game over');
         }
-        touch++ 
-        if (touch==round) {
-            //We win a round
-            touch=0; //we reset the touch to the new round
-            round++;//It increase the level
-            randomNumbers=[];//reset 
-            mixtColors()
-            showColors()
-            console.log(randomNumbers);
-        }
-        
+    }else if (e.target.classList.contains('button--start')) {
+        reset(1);
     }
 })
 
-mixtColors()
-showColors()
-console.log(randomNumbers);
+const reset=(roundParameter)=>{
+    //We change the level in the html
+    level.textContent=`Level ${roundParameter}`
+    round=roundParameter; 
+    touch=0;
+    randomNumbers=[];
+    mixColors();
+    showColors();
+    console.log(randomNumbers);
+}
+
